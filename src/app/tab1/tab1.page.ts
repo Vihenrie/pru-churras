b66f280a29e1341e289ccc1e674f9d9be3d82469
Vinicius Henrique Santos Pinto <viniciushpintolp@gmail.com>
2023-03-09 12:23:53 -0300

    att pru-churras

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,9 +1,18 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
+  animations: [
+    trigger('rotatedState', [
+      state('default', style({ transform: 'rotateY(o)'})),
+      state('rotated', style({ transform: 'rotateY(-360deg)'})),
+      transition('rotated => default', animate('2000ms ease-out')),
+      transition('default => rotated', animate('2000ms ease-in'))
+    ])
+  ]
 })
 export class Tab1Page {
 
@@ -12,18 +21,23 @@ export class Tab1Page {
   logo = 'assets/logo.png';
   image = this.logo;
   info = 'clique no botão para jogar!';
-
+  state = 'default';
 
   constructor() {}
 
   jogarMoeda(){
-    if(Math.random() <0.5){
-      this.image = this.cara;
-      this.info = "cara!"
-    }else{
-      this.image = this.coroa;
-      this.info = "Coroa!"
-    }
-  }
+    this.info = 'Girando...';
+    this.image = this.logo;
+    this.state = this.state === 'default' ? 'rotated' : 'default';
 
+    setTimeout(() => {
+      if(Math.random() <0.5){
+        this.image = this.cara;
+        this.info = "cara!"
+      }else{
+        this.image = this.coroa;
+        this.info = "Coroa!"
+      }
+    },2000);
+  }
 }
