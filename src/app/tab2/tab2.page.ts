31d2e6f0561ec9688ed28e60152593c2b7aa3662
Vihenrie <viniciushpintolp@gmail.com>
2023-03-13 18:43:00 -0300

    att pru churras

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -7,21 +7,23 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
-
   pontos: number = 1;
-  somapontos: number = 0;
   time1: number = 0;
   time2: number = 0;
 
+  constructor() {}
+
   adicionarPontos(valor: number) {
     this.pontos = valor;
+  }
+
+  somapontos(valor: number) {
+    valor += this.pontos;
   }
 
   zerarPontos() {
     this.pontos = 0;
     this.time1 = 0;
     this.time2 = 0;
-    this.somapontos = 0;
   }
 }
