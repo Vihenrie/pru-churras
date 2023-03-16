abc7b34908cca1aab2f3059e1b87f8df626e45e7
Vinicius Henrique Santos Pinto <viniciushpintolp@gmail.com>
2023-03-16 12:28:41 -0300

    contador de truco

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -8,6 +8,10 @@ import { Component } from '@angular/core';
 export class Tab2Page {
 
   pontos: number = 1;
+
+  pontosatual: number = 0;
+  pontosatual2: number = 0;
+
   time1: number = 0;
   time2: number = 0;
 
@@ -17,13 +21,48 @@ export class Tab2Page {
     this.pontos = valor;
   }
 
-  somapontos(valor: number) {
-    valor += this.pontos;
+  somapontos() {
+    this.time1 = this.pontos + this.time1;
+    if(this.time1 >= 12) {
+      this.pontosatual++
+      this.recomecar();
+    }
+  }
+
+  somapontos2() {
+    this.time2 = this.pontos + this.time2;
+    if(this.time2 >= 12) {
+      this.pontosatual2++
+      this.recomecar();
+    }
+  }
+
+  subpontos() {
+    this.time1 = this.time1 - this.pontos;
+    if(this.time1 <= 0){
+      this.time1 = 0;
+    }
+  }
+
+  subpontos2() {
+    this.time2 = this.time2 - this.pontos;
+    if(this.time2 <= 0){
+      this.time2 = 0;
+    }
+  }
+
+  recomecar(){
+    this.pontos = 1;
+    this.time1 = 0;
+    this.time2 = 0;
   }
 
   zerarPontos() {
-    this.pontos = 0;
+    this.pontos = 1;
     this.time1 = 0;
     this.time2 = 0;
+    this.pontosatual = 0;
+    this.pontosatual2 = 0;
+
   }
 }
